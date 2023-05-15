564d70206b2a5208c737fbc4ef5d1317759ae5fe
Coral <cnight18@hotmail.com>
2023-05-15 10:14:02 -0300

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
-// Assignment Code
+// generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// variables
+var generatePassword = "";
+var generateOutput = "";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const specialChars = "{[!@#$%^&*()_+=-~`<>?/]}";
+const numbers = "0987654321";
+
+
+
+// code to start 
+function generatePassword() {
+ var generatePassword = "";
+ var generateOutput = "";
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+
+
+
+
+
+
 
 }
 
